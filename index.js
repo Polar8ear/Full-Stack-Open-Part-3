da455d620b9c2ae22593e9fccd841806a7cd3d28
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-07-08 14:50:06 +0800

    Part 3.2

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ app.get('/api/persons',(request,response) =>{
     response.json(persons)
 })
 
+app.get('/info',(request,response) =>{
+    const message = `<div>Phonebook has info for ${persons.length} people</div>
+                    <div>${new Date()}</div>`
+    response.send(message)
+})
+
 const PORT=3001
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
