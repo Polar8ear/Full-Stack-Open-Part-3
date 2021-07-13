ab432448ee071a1e832b8f76eb941226e8ae441f
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-07-13 08:46:09 +0800

    Part 3.15 Added functionality 'delete person'

--- index.js
+++ index.js
@@ -15,29 +15,6 @@ app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 
 app.get('/api/persons',(request,response) =>{
     Person.find({}).then(persons=>response.json(persons))
@@ -69,18 +46,18 @@ app.post('/api/persons',(request,response) =>{
 
 
 app.delete('/api/persons/:id',(request,response)=>{
-  const id=Number(request.params.id)
-    persons=persons.filter(person=>person.id!==id)
-
-    response.status(204).end()
+  const id=request.params.id
+    Person.findByIdAndRemove(id)
+          .then(result=>response.status(204).end())
+          .catch(error=>next(error))
 })
 
-app.get('/info',(request,response) =>{
-    const message = `<div>Phonebook has info for ${persons.length} people</div>
-                    <div>${new Date()}</div>`      
+// app.get('/info',(request,response) =>{
+//     const message = `<div>Phonebook has info for ${persons.length} people</div>
+//                     <div>${new Date()}</div>`      
 
-    response.send(message)
-})
+//     response.send(message)
+// })
 
 const PORT= process.env.PORT
 app.listen(PORT,()=>{
